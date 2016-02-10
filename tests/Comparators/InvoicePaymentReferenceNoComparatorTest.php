4761b662145a0360ebd7e9549830cccc80a3300e
markus <markus.karileet@codehouse.ee>
2016-02-10 09:12:50 +0200

    implemented comparing tests

--- tests/Comparators/InvoicePaymentReferenceNoComparatorTest.php
+++ tests/Comparators/InvoicePaymentReferenceNoComparatorTest.php
@@ -15,12 +15,29 @@ class InvoicePaymentReferenceNoComparatorTest extends PHPUnit_Framework_TestCase
 
     public function testShouldMatchOne()
     {
-
+        $invoices = new \ArrayObject();
+        $invoice = new \Entities\SampleInvoice('48,00', '905/IT', '2016020508', '');
+        $invoices->append($invoice);
+        $payments = new \ArrayObject();
+        $payments->append(new \Entities\Payment('48,00', 'RO1755898361', 'Arve nr. 905/IT', 'Company OÜ'));
+        $payments->append(new \Entities\Payment('15,00', 'RO1756686260', 'Tellimus 365/PF', 'Firma AS'));
+        $payments->append(new \Entities\Payment('85,20', 'RO1757162444', 'tellimus 353/PF 24,12,15', 'ISA JA POJAD OÜ'));
+        $mapped = $this->comparator->compare($invoices, $payments);
+        $this->assertEquals(1, $mapped->count());
+        $this->assertEquals($invoice, $mapped->offsetGet(0)->getRelatedInvoice());
     }
 
     public function testShouldMatchNone()
     {
-
+        $invoices = new \ArrayObject();
+        $invoice = new \Entities\SampleInvoice('100', '666/AA', '2015120508', '');
+        $invoices->append($invoice);
+        $payments = new \ArrayObject();
+        $payments->append(new \Entities\Payment('48,00', 'RO1755898361', 'Arve nr. 905/IT', 'Company OÜ'));
+        $payments->append(new \Entities\Payment('15,00', 'RO1756686260', 'Tellimus 365/PF', 'Firma AS'));
+        $payments->append(new \Entities\Payment('85,20', 'RO1757162444', 'tellimus 353/PF 24,12,15', 'ISA JA POJAD OÜ'));
+        $mapped = $this->comparator->compare($invoices, $payments);
+        $this->assertEquals(0, $mapped->count());
     }
 
     /**
