140273fd3ed3d523a0950641ac6655703eb60a67
markus <markus.karileet@codehouse.ee>
2016-02-10 09:19:30 +0200

    implemented comparator in mapper

--- src/Mappers/MapperImpl.php
+++ src/Mappers/MapperImpl.php
@@ -2,7 +2,8 @@
 
 namespace Mappers;
 
-use Entities\Invoice;
+use Comparators\ComparatorFactory;
+use Comparators\ComparatorFactoryImpl;
 use Extractors\ExtractorFactory;
 use Extractors\ExtractorFactoryImpl;
 
@@ -25,6 +26,10 @@ class MapperImpl implements Mapper
      * @var ExtractorFactory
      */
     private $_extractorFactory;
+    /**
+     * @var ComparatorFactory
+     */
+    private $_comparatorFactory;
 
     /**
      * MapperImpl constructor.
@@ -33,6 +38,7 @@ class MapperImpl implements Mapper
     {
         $this->_logger = new \Logger(get_class($this));
         $this->setExtractorFactory(new ExtractorFactoryImpl());
+        $this->setComparatorFactory(new ComparatorFactoryImpl());
     }
 
     /**
@@ -46,45 +52,35 @@ class MapperImpl implements Mapper
     }
 
     /**
+     * Set current comparator factory
+     *
+     * @param ComparatorFactory $factory Comparator factory to use
+     */
+    public function setComparatorFactory($factory)
+    {
+        $this->_comparatorFactory = $factory;
+    }
+
+    /**
      * Map $invoices with Payments that are retrieved from $pathToFile using $parserName parser
      *
      * @param \ArrayObject $invoices Invoices to check
      * @param string $pathToSource Absolute path source, that contains payments
      * @param string $parserName Name of the parser to use while parsing file (see ParserFactory class)
+     * @param string $comparatorName Name of the comparator, default is 'IPRNo'
      * @param string $extractorName Name of the extractor to use, default is 'File'
      *
      * @return \ArrayObject List of invoices that were paid
      * @throws \Exception When file parsing fails, exception is thrown
      */
-    public function map(\ArrayObject $invoices, $pathToSource, $parserName, $extractorName = 'File')
+    public function map(\ArrayObject $invoices, $pathToSource, $parserName, $comparatorName = 'IPRNo', $extractorName = 'File')
     {
-        $results = new \ArrayObject();
         $extractor = $this->_extractorFactory->getExtractor($extractorName);
+        $comparator = $this->_comparatorFactory->getComparator($comparatorName);
+
         $extractor->extractPayments($pathToSource, $parserName);
         $payments = $extractor->getPayments();
-        foreach ($payments as $payment) {
-            foreach ($invoices as $invoice) {
-                if ($invoice instanceof Invoice) {
-                    $invoiceNoMatch = false;
-                    if ($invoice->getInvoiceNo() != '') {
-                        $invoiceNoMatch = strpos($payment->getDescription(), $invoice->getInvoiceNo()) !== false;
-                    }
-                    $orderNoMatch = false;
-                    if ($invoice->getOrderNo() != '') {
-                        $orderNoMatch = strpos($payment->getDescription(), $invoice->getOrderNo()) !== false;
-                    }
-                    if (($payment->getReferenceNo() == $invoice->getReferenceNo() && $payment->getReferenceNo() != '')
-                        || ($orderNoMatch || $invoiceNoMatch)
-                    ) {
-                        $payment->setRelatedInvoice($invoice);
-                        $results->append($payment);
-                    }
-                } else {
-                    throw new \Exception("Invoice not implementing correct interface!");
-                }
-
-            }
-        }
+        $results = $comparator->compare($invoices, $payments);
         return $results;
     }
 
